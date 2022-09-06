766effde9e7a8ed5ad57200ebf2fdd59c70398c5
nouherg <nouhereguigui@gmail.com>
2022-09-06 13:00:22 +0200

    first

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const connectDB = require("./config/connectDB");
 const user = require("./model/User");
 const app = express();
 const PORT = process.env.PORT;
-
+const userRoutes = require("./route/user");
 
 app.use(express.json());
 
+app.use("/api/users", userRoutes);
 connectDB();
 
 
